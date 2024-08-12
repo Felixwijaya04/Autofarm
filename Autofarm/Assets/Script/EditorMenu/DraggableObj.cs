6d0cfa79a0dbb58b5f1f110641e7a408fd0138a2
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-08-12 18:36:38 +0700

    drag and drop init

---
+++ Autofarm/Assets/Script/EditorMenu/DraggableObj.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableObj : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject TypeOfCmd; // Prefab to be cloned
+    private GameObject duplicatedObj;
+    private Canvas parentCanvas;
+
+    private void Start()
+    {
+        parentCanvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        duplicatedObj = Instantiate(TypeOfCmd, transform.position, Quaternion.identity, parentCanvas.transform);
+        if (duplicatedObj != null)
+        {
+            duplicatedObj.SetActive(true);
+            Debug.Log("Duplicated Object Position: " + duplicatedObj.transform.position);
+        }
+        else
+        {
+            Debug.LogError("Failed to instantiate duplicated object!");
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        if (duplicatedObj != null)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(parentCanvas.transform as RectTransform, Input.mousePosition, parentCanvas.worldCamera, out Vector2 localPoint);
+            duplicatedObj.GetComponent<RectTransform>().localPosition = localPoint;
+            Debug.Log("Dragging Position: " + duplicatedObj.transform.localPosition);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
+}
