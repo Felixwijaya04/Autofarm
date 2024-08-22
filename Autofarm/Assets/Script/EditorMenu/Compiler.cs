5823d488fb62da0a4dde8fe3575d875cf994b911
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-08-22 19:30:05 +0700

    bug fixes compiler

--- Autofarm/Assets/Script/EditorMenu/Compiler.cs
+++ Autofarm/Assets/Script/EditorMenu/Compiler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public abstract class BaseFunction : MonoBehaviour
 {
@@ -10,15 +11,17 @@ public class Compiler : MonoBehaviour
 {
     public RectTransform[] codeSlots;
     [SerializeField] private LevelMaker[] levelMaker;
-    public int levels = 0;
+    [HideInInspector]public int levels = 0;
 
     public void checkAnswer()
     {
         bool allCorrect = true;
-
-        for (int i = 0; i < codeSlots.Length; i++)
+        int len = levelMaker[levels].ExpectedFunctions.Count;
+        Debug.Log("len"+ len);
+        for (int i = 0; i < len; i++)
         {
             BaseFunction attachedFunction = codeSlots[i].GetComponentInChildren<BaseFunction>();
+            Debug.Log("Attached Function at slot " + i + ": " + (attachedFunction != null ? attachedFunction.GetType().Name : "null"));
             if (attachedFunction != null)
             {
                 if (attachedFunction.GetType() == levelMaker[levels].ExpectedFunctions[i].GetType())
