d7f5f75b51fd23c9a2ac1fd776009b925444e1da
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-08-22 19:04:35 +0700

    ..

--- Autofarm/Assets/Script/EditorMenu/Compiler.cs
+++ Autofarm/Assets/Script/EditorMenu/Compiler.cs
@@ -11,36 +11,9 @@ public class Compiler : MonoBehaviour
     public RectTransform[] codeSlots;
     [SerializeField] private LevelMaker[] levelMaker;
     public int levels = 0;
-    //private bool NextLevel = false;
 
     public void checkAnswer()
     {
-        /*for(int i=0;i<codeSlots.Length; i++)
-        {
-            BaseFunction attachedFunction = codeSlots[i].GetComponentInChildren<BaseFunction>();
-            if(attachedFunction != null)
-            {
-                if (attachedFunction.GetType() == levelMaker[levels].ExpectedFunctions[i].GetType())
-                {
-                    Debug.Log("benar " + levels);
-                    NextLevel = true;
-                }
-                else
-                {
-                    Debug.Log("salah " + levels);
-                    NextLevel = false;
-                }
-            }
-            else
-            {
-                Debug.Log("incomplete answer " + levels);
-                NextLevel = false;
-            }
-        }
-        if(NextLevel == true)
-        {
-            levels++;
-        }*/
         bool allCorrect = true;
 
         for (int i = 0; i < codeSlots.Length; i++)
